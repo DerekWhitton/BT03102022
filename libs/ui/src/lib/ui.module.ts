fc9504d5fb06bb571427be27828e586d68b26989
Muhammad Desai <moe@temple.co.za>
2020-10-27 15:48:57 +0200

    added product item component to ui lib

--- libs/ui/src/lib/ui.module.ts
+++ libs/ui/src/lib/ui.module.ts
@@ -4,9 +4,10 @@ import { PortalMenuComponent } from './portal-menu/portal-menu.component';
 import { CrudTableComponent } from './crud-table/crud-table.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { ProductItemComponent } from './product-item/product-item.component';
 
 @NgModule({
   imports: [CommonModule],
-  declarations: [PortalMenuComponent, CrudTableComponent, HeaderComponent, FooterComponent],
+  declarations: [PortalMenuComponent, CrudTableComponent, HeaderComponent, FooterComponent, ProductItemComponent],
 })
 export class UiModule {}
