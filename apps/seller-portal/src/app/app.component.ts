61de0d81c95bb130af8fbaa2f0bca15254b88053
Muhammad Desai <moe@temple.co.za>
2020-10-27 15:16:17 +0200

    added seller portal app

---
+++ apps/seller-portal/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'bushtrade.web-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'seller-portal';
+}
--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,6 @@
 module.exports = {
-  projects: ['<rootDir>/apps/administration-portal'],
+  projects: [
+    '<rootDir>/apps/administration-portal',
+    '<rootDir>/apps/seller-portal',
+  ],
 };
