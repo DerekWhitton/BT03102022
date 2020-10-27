07863b0ff2ed42c38e33ff0d77823b9108a43c10
Muhammad Desai <moe@temple.co.za>
2020-10-27 15:46:14 +0200

    added header and footer to ui lib

--- libs/ui/src/lib/ui.module.ts
+++ libs/ui/src/lib/ui.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PortalMenuComponent } from './portal-menu/portal-menu.component';
 import { CrudTableComponent } from './crud-table/crud-table.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   imports: [CommonModule],
-  declarations: [PortalMenuComponent, CrudTableComponent],
+  declarations: [PortalMenuComponent, CrudTableComponent, HeaderComponent, FooterComponent],
 })
 export class UiModule {}
