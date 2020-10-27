08b5d79fcf7498146440d0d167b8918658c7cecc
Muhammad Desai <moe@temple.co.za>
2020-10-27 15:30:38 +0200

    added crud table component to ui lib

--- libs/ui/src/lib/ui.module.ts
+++ libs/ui/src/lib/ui.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PortalMenuComponent } from './portal-menu/portal-menu.component';
+import { CrudTableComponent } from './crud-table/crud-table.component';
 
 @NgModule({
   imports: [CommonModule],
-  declarations: [PortalMenuComponent],
+  declarations: [PortalMenuComponent, CrudTableComponent],
 })
 export class UiModule {}
