9d89a07766a60335459ef2eb55c76e91413f1e75
Muhammad Desai <moe@temple.co.za>
2020-10-27 15:26:54 +0200

    added portal menu component in ui lib

---
+++ libs/ui/src/lib/portal-menu/portal-menu.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'bushtrade.web-portal-menu',
+  templateUrl: './portal-menu.component.html',
+  styleUrls: ['./portal-menu.component.scss']
+})
+export class PortalMenuComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- libs/ui/src/lib/ui.module.ts
+++ libs/ui/src/lib/ui.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { PortalMenuComponent } from './portal-menu/portal-menu.component';
 
 @NgModule({
   imports: [CommonModule],
+  declarations: [PortalMenuComponent],
 })
 export class UiModule {}
