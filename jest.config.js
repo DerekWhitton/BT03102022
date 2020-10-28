f4322ad66c24509a0578dfad7da8ab3a61b69388
Muhammad Desai <moe@temple.co.za>
2020-10-28 10:08:52 +0200

    added messages lib

--- jest.config.js
+++ jest.config.js
@@ -7,5 +7,6 @@ module.exports = {
     '<rootDir>/libs/seller-portal/listings',
     '<rootDir>/libs/seller-portal/banking',
     '<rootDir>/libs/seller-portal/sales',
+    '<rootDir>/libs/seller-portal/messages',
   ],
 };
