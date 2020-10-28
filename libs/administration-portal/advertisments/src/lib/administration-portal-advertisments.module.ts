ee77aa551c157b6f6bcfde28bad1a6683eecad7c
Muhammad Desai <moe@temple.co.za>
2020-10-28 11:40:20 +0200

    added avertisements index components

--- libs/administration-portal/advertisments/src/lib/administration-portal-advertisments.module.ts
+++ libs/administration-portal/advertisments/src/lib/administration-portal-advertisments.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Route } from '@angular/router';
+import { AdvertisementsIndexComponent } from './containers/advertisements-index/advertisements-index.component';
 
 export const administrationPortalAdvertismentsRoutes: Route[] = [];
 
 @NgModule({
   imports: [CommonModule, RouterModule],
+  declarations: [AdvertisementsIndexComponent],
+  entryComponents: [AdvertisementsIndexComponent],
 })
 export class AdministrationPortalAdvertismentsModule {}
---
+++ libs/administration-portal/advertisments/src/lib/containers/advertisements-index/advertisements-index.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'bushtrade.web-advertisements-index',
+  templateUrl: './advertisements-index.component.html',
+  styleUrls: ['./advertisements-index.component.scss']
+})
+export class AdvertisementsIndexComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
