f41180aaef242e94556f87aee847349714381443
Muhammad Desai <moe@temple.co.za>
2020-10-28 11:42:04 +0200

    added articles index component

--- libs/administration-portal/advertisments/src/lib/containers/advertisements-index/advertisements-index.component.ts
+++ libs/administration-portal/advertisments/src/lib/containers/advertisements-index/advertisements-index.component.ts
@@ -1,15 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'bushtrade.web-advertisements-index',
+  selector: 'bushtrade-administration-advertisements-index',
   templateUrl: './advertisements-index.component.html',
-  styleUrls: ['./advertisements-index.component.scss']
+  styleUrls: ['./advertisements-index.component.scss'],
 })
 export class AdvertisementsIndexComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
--- libs/administration-portal/articles/src/lib/administration-portal-articles.module.ts
+++ libs/administration-portal/articles/src/lib/administration-portal-articles.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Route } from '@angular/router';
+import { ArticlesIndexComponent } from './articles-index/articles-index.component';
 
 export const administrationPortalArticlesRoutes: Route[] = [];
 
 @NgModule({
   imports: [CommonModule, RouterModule],
+  declarations: [ArticlesIndexComponent],
+  entryComponents: [ArticlesIndexComponent],
 })
 export class AdministrationPortalArticlesModule {}
