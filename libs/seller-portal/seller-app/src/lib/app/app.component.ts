814cc6efdee9624bb0e28f38f5fc192a1dcdb162
Muhammad Desai <moe@temple.co.za>
2020-10-28 14:17:21 +0200

    added selelr app

---
+++ libs/seller-portal/seller-app/src/lib/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'bushtrade-seller-portal-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- libs/seller-portal/seller-app/src/lib/seller-portal-seller-app.module.ts
+++ libs/seller-portal/seller-app/src/lib/seller-portal-seller-app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { AppComponent } from './app/app.component';
 
 @NgModule({
   imports: [
@@ -10,5 +11,7 @@ import { RouterModule } from '@angular/router';
       /* {path: '', pathMatch: 'full', component: InsertYourComponentHere} */
     ]),
   ],
+  declarations: [AppComponent],
+  entryComponents: [AppComponent],
 })
 export class SellerPortalSellerAppModule {}
