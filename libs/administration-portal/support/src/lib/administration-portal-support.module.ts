f0e4e16d0660ae7f73a28fcc72294ec17be07d58
Muhammad Desai <moe@temple.co.za>
2020-10-28 11:51:15 +0200

    container for support index

--- libs/administration-portal/support/src/lib/administration-portal-support.module.ts
+++ libs/administration-portal/support/src/lib/administration-portal-support.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Route } from '@angular/router';
-import { SupportIndexComponent } from './support-index/support-index.component';
+import { SupportIndexComponent } from './containers/support-index/support-index.component';
 
 export const administrationPortalSupportRoutes: Route[] = [];
 
