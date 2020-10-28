dad81c9387d068eac0e1ed9a204a007d179a224b
Muhammad Desai <moe@temple.co.za>
2020-10-28 14:29:01 +0200

    added sales index component

--- libs/seller-portal/sales/src/lib/seller-portal-sales.module.ts
+++ libs/seller-portal/sales/src/lib/seller-portal-sales.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Route } from '@angular/router';
+import { SalesIndexComponent } from './containers/sales-index/sales-index.component';
 
 export const sellerPortalSalesRoutes: Route[] = [];
 
 @NgModule({
   imports: [CommonModule, RouterModule],
+  declarations: [SalesIndexComponent],
+  entryComponents: [SalesIndexComponent],
 })
 export class SellerPortalSalesModule {}
