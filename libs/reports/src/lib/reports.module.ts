58b658c0514de468becf3ec4c8f560378054c703
Muhammad Desai <moe@temple.co.za>
2020-10-28 09:55:12 +0200

    added reports lib

---
+++ libs/reports/src/lib/reports.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Route } from '@angular/router';
+
+export const reportsRoutes: Route[] = [];
+
+@NgModule({
+  imports: [CommonModule, RouterModule],
+})
+export class ReportsModule {}
