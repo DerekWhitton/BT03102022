177fe15662b1c3139fd28ea766a84f7cc0f9c5f1
Muhammad Desai <moe@temple.co.za>
2020-10-28 11:15:02 +0200

    updated ui lib module

--- libs/ui/src/lib/ui.module.ts
+++ libs/ui/src/lib/ui.module.ts
@@ -1,13 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MegaMenuModule } from 'primeng/megamenu';
 import { PortalMenuComponent } from './portal-menu/portal-menu.component';
 import { CrudTableComponent } from './crud-table/crud-table.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
-import { ProductItemComponent } from './product-item/product-item.component';
 
 @NgModule({
-  imports: [CommonModule],
-  declarations: [PortalMenuComponent, CrudTableComponent, HeaderComponent, FooterComponent, ProductItemComponent],
+  imports: [CommonModule, MegaMenuModule],
+  declarations: [
+    PortalMenuComponent,
+    CrudTableComponent,
+    HeaderComponent,
+    FooterComponent,
+  ],
+  exports: [
+    PortalMenuComponent,
+    CrudTableComponent,
+    HeaderComponent,
+    FooterComponent,
+  ],
 })
 export class UiModule {}
