ff08eda615aa4a00d4ce0ff165902c3cc6649be4
Muhammad Desai <moe@temple.co.za>
2020-10-28 11:45:33 +0200

    added forums index component

--- libs/administration-portal/forums/src/lib/administration-portal-forums.module.ts
+++ libs/administration-portal/forums/src/lib/administration-portal-forums.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Route } from '@angular/router';
+import { ForumIndexComponent } from './containers/forum-index/forum-index.component';
 
 export const administrationPortalForumsRoutes: Route[] = [];
 
 @NgModule({
   imports: [CommonModule, RouterModule],
+  declarations: [ForumIndexComponent],
+  entryComponents: [ForumIndexComponent],
 })
 export class AdministrationPortalForumsModule {}
