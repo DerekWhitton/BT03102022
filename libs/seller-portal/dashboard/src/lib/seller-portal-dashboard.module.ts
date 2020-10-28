e3cbbd1880cdfd9260abe23d231595be89c7fca0
Muhammad Desai <moe@temple.co.za>
2020-10-28 14:33:50 +0200

    added dashboard index component

--- libs/seller-portal/dashboard/src/lib/seller-portal-dashboard.module.ts
+++ libs/seller-portal/dashboard/src/lib/seller-portal-dashboard.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { DashboardIndexComponent } from './containers/dashboard-index/dashboard-index.component';
 
 @NgModule({
   imports: [CommonModule],
+  declarations: [DashboardIndexComponent],
+  entryComponents: [DashboardIndexComponent],
 })
 export class SellerPortalDashboardModule {}
