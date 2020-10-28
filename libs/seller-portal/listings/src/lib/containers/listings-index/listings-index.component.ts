cf5699e8898c5d4c7ea3d51d67ccf44df01b9dd6
Muhammad Desai <moe@temple.co.za>
2020-10-28 14:26:03 +0200

    added listings component

---
+++ libs/seller-portal/listings/src/lib/containers/listings-index/listings-index.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'bushtrade-seller-listings-index',
+  templateUrl: './listings-index.component.html',
+  styleUrls: ['./listings-index.component.scss']
+})
+export class ListingsIndexComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
