eee079ac76b3c70cf49a86ace07a51144d7be2d4
Muhammad Desai <moe@temple.co.za>
2020-10-28 11:38:06 +0200

    setup menu and router

--- libs/administration-portal/administration-app/src/lib/administration-portal-administration-app.module.ts
+++ libs/administration-portal/administration-app/src/lib/administration-portal-administration-app.module.ts
@@ -7,8 +7,71 @@ import { UiModule } from '@bushtrade/ui';
   imports: [
     CommonModule,
     UiModule,
-    RouterModule.forChild([
+    RouterModule.forRoot([
       { path: '', pathMatch: 'full', component: IndexComponent },
+      {
+        path: 'users',
+        loadChildren: () =>
+          import('@bushtrade/administration-portal/users').then(
+            (module) => module.AdministrationPortalUsersModule
+          ),
+      },
+      {
+        path: 'advertisments',
+        loadChildren: () =>
+          import('@bushtrade/administration-portal/advertisments').then(
+            (module) => module.AdministrationPortalAdvertismentsModule
+          ),
+      },
+      {
+        path: 'articles',
+        loadChildren: () =>
+          import('@bushtrade/administration-portal/articles').then(
+            (module) => module.AdministrationPortalArticlesModule
+          ),
+      },
+      {
+        path: 'escrow',
+        loadChildren: () =>
+          import('@bushtrade/administration-portal/escrow').then(
+            (module) => module.AdministrationPortalEscrowModule
+          ),
+      },
+      {
+        path: 'forums',
+        loadChildren: () =>
+          import('@bushtrade/administration-portal/forums').then(
+            (module) => module.AdministrationPortalForumsModule
+          ),
+      },
+      {
+        path: 'listings',
+        loadChildren: () =>
+          import('@bushtrade/administration-portal/listings').then(
+            (module) => module.AdministrationPortalListingsModule
+          ),
+      },
+      {
+        path: 'messages',
+        loadChildren: () =>
+          import('@bushtrade/administration-portal/messages').then(
+            (module) => module.AdministrationPortalMessagesModule
+          ),
+      },
+      {
+        path: 'reports',
+        loadChildren: () =>
+          import('@bushtrade/administration-portal/reports').then(
+            (module) => module.AdministrationPortalReportsModule
+          ),
+      },
+      {
+        path: 'support',
+        loadChildren: () =>
+          import('@bushtrade/administration-portal/support').then(
+            (module) => module.AdministrationPortalSupportModule
+          ),
+      },
     ]),
   ],
   declarations: [IndexComponent],
--- libs/administration-portal/administration-app/src/lib/index/index.component.ts
+++ libs/administration-portal/administration-app/src/lib/index/index.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MegaMenuItem } from 'primeng/api';
 
 @Component({
   selector: 'bushtrade-aministration-index',
@@ -6,7 +7,60 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./index.component.scss'],
 })
 export class IndexComponent implements OnInit {
+  menuItems: MegaMenuItem[];
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.menuItems = [
+      {
+        label: 'Dashboard',
+        icon: 'pi pi-pw pi-chart-bar',
+      },
+      {
+        label: 'Users',
+        icon: 'pi pi-pw pi-cog',
+        routerLink: ['/', 'users'],
+      },
+      {
+        label: 'Advertisments',
+        icon: 'pi pi-pw pi-envelope',
+        routerLink: ['/', 'advertisments'],
+      },
+      {
+        label: 'Articles',
+        icon: 'pi pi-pw pi-table',
+        routerLink: ['/', 'articles'],
+      },
+      {
+        label: 'Escrow',
+        icon: 'pi pi-pw pi-shopping-cart',
+        routerLink: ['/', 'escrow'],
+      },
+      {
+        label: 'Forums',
+        icon: 'pi pi-pw pi-user',
+        routerLink: ['/', 'forums'],
+      },
+      {
+        label: 'Listings',
+        icon: 'pi pi-pw pi-comments',
+        routerLink: ['/', 'listings'],
+      },
+      {
+        label: 'Messages',
+        icon: 'pi pi-pw pi-calendar',
+        routerLink: ['/', 'messages'],
+      },
+      {
+        label: 'Reports',
+        icon: 'pi pi-pw pi-globe',
+        routerLink: ['/', 'reports'],
+      },
+      {
+        label: 'Support',
+        icon: 'pi pi-pw pi-cog',
+        routerLink: ['/', 'support'],
+      },
+    ];
+  }
 }
