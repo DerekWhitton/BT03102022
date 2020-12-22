6c6e502c6a1faa4269238a06ad0b6ceb6ba9b71b
Muhammad Desai <moe@temple.co.za>
2020-12-22 13:47:34 +0200

    logged in service

--- libs/website/marketplace/src/lib/marketplace-index/marketplace-index.component.ts
+++ libs/website/marketplace/src/lib/marketplace-index/marketplace-index.component.ts
@@ -1,15 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-
+import { UserService } from '@bushtrade/website/shared/services';
 @Component({
   selector: 'bushtrade-web-marketplace-index',
   templateUrl: './marketplace-index.component.html',
-  styleUrls: ['./marketplace-index.component.scss']
+  styleUrls: ['./marketplace-index.component.scss'],
 })
 export class MarketplaceIndexComponent implements OnInit {
-
-  constructor() { }
+  constructor(public userService: UserService) {}
 
   ngOnInit(): void {
+    console.log(this.userService.loggedIn());
   }
-
 }
--- libs/website/shared/services/src/lib/user/user.service.ts
+++ libs/website/shared/services/src/lib/user/user.service.ts
@@ -2,6 +2,7 @@ import { Inject, Injectable } from '@angular/core';
 import { APP_CONFIG } from '@bushtrade/app-config';
 import { HttpClient, HttpResponse } from '@angular/common/http';
 import { ISeller, IUser } from '@bushtrade/website/shared/entites';
+import { MsalService } from '@azure/msal-angular';
 @Injectable({
   providedIn: 'root',
 })
@@ -10,11 +11,17 @@ export class UserService {
   private version: string;
   constructor(
     @Inject(APP_CONFIG) private configuration: any,
-    private httpClient: HttpClient
+    private httpClient: HttpClient,
+    private authService: MsalService
   ) {
     this.base = configuration.apiRoute;
     this.version = configuration.apiVersion;
   }
+
+  loggedIn() {
+    return this.authService.getAccount() !== null ? true : false;
+  }
+
   loadUser() {
     return this.httpClient.get<IUser>(
       `${this.base}api/v${this.version}/Profiles/Me`
