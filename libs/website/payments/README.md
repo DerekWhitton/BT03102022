68b45c880a2ddd84f18d8ab71be3ef54679caa7e
Jaco Jansen van Vuuren <hello@jacojvv.dev>
2021-02-15 08:37:34 +0200

    Added checkout redirection pages

---
+++ libs/website/payments/README.md
@@ -0,0 +1,7 @@
+# website-payments
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test website-payments` to execute the unit tests.
---
+++ libs/website/payments/src/lib/containers/cancelled/cancelled.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'bushtrade.web-cancelled',
+  templateUrl: './cancelled.component.html',
+  styleUrls: ['./cancelled.component.scss']
+})
+export class CancelledComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
