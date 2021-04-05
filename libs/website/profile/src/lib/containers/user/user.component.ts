3affea323aac5520b9181b2e8a7dc6afd22fc00c
Kevin Bonkewitzz <kevin@temple.co.za>
2021-04-05 22:52:15 +0200

    Display Supplier Profile Picture

--- libs/website/profile/src/lib/containers/user/user.component.ts
+++ libs/website/profile/src/lib/containers/user/user.component.ts
@@ -1,8 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
 import { IUser } from '@bushtrade/website/shared/entites';
-import { getUser, loadUser } from '@bushtrade/website/shared/state';
+import { UserService } from '@bushtrade/website/shared/services';
+import { getUser, loadUser, setProfilePicture } from '@bushtrade/website/shared/state';
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
 
 @Component({
   selector: 'website-user-index',
@@ -12,10 +15,38 @@ import { Observable } from 'rxjs';
 export class UserComponent implements OnInit {
   user$: Observable<IUser>;
   loaded$: Observable<boolean>;
-  constructor(private store: Store) {}
+  image: SafeUrl = "assets/layout/images/no-profile.png";
+  @ViewChild('fileUpload') fileUpload: any;
+
+  constructor(
+    private store: Store, 
+    private userService: UserService,
+    private sanitizer: DomSanitizer) {}
 
   ngOnInit(): void {
     this.store.dispatch(loadUser());
     this.user$ = this.store.select(getUser);
+
+    // Display profile image only if provided
+    this.user$.subscribe(user => {
+      if (user.profilePictureUri.length > 0)
+          this.image = user.profilePictureUri;
+    })
+    
+  }
+
+  UploadProfilePicture(event: { files: any }) {
+    // Update the image on the UI
+    this.image = this.sanitizer.bypassSecurityTrustUrl(
+      window.URL.createObjectURL(event.files[0])
+    );
+    
+    // POST the new image to server and retrieve it's Guid
+    this.userService.uploadProfilePicture(event.files[0]).subscribe(imageId => {
+      this.store.dispatch(setProfilePicture({ filePath: this.image as string }))
+    });
+    
+    // Reset the file uploader to prevent stacking
+    this.fileUpload.clear();
   }
 }
--- libs/website/shared/state/src/lib/user/user.effects.ts
+++ libs/website/shared/state/src/lib/user/user.effects.ts
@@ -1,10 +1,10 @@
 import { Injectable } from '@angular/core';
-import { Actions, createEffect, ofType, OnInitEffects } from '@ngrx/effects';
-import { map, withLatestFrom } from 'rxjs/operators';
+import { Actions, createEffect, ofType } from '@ngrx/effects';
+import { catchError, exhaustMap, map } from 'rxjs/operators';
 import { fetch } from '@nrwl/angular';
 import { UserService } from '@bushtrade/website/shared/services';
 import * as UserActions from './user.actions';
-import { Action } from '@ngrx/store';
+import { of } from 'rxjs';
 
 @Injectable()
 export class UserEffects {
@@ -50,9 +50,19 @@ export class UserEffects {
     )
   );
 
-  constructor(private actions$: Actions, private userService: UserService) {}
+  updateProfilePicture$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(UserActions.setProfilePicture),
+      fetch({
+        run: (action) => { 
+          return UserActions.setProfilePictureSuccess({ filePath: action.filePath }) 
+        },
+        onError: (action, error) => {
+          return UserActions.setProfilePictureFailure({ error });
+        },
+      })
+    )
+  );
 
-  // ngrxOnInitEffects(): Action {
-  //   return UserActions.loadUser();
-  // }
+  constructor(private actions$: Actions, private userService: UserService) {}
 }
