dce043602176bf8032fc97932e5296db3f3bd9cc
vladmarginean <vladmarginean9@gmail.com>
2021-09-20 18:24:02 +0300

    Modified image references to account for resized images

--- libs/ui/src/lib/website-menu/website-menu.component.ts
+++ libs/ui/src/lib/website-menu/website-menu.component.ts
@@ -45,8 +45,11 @@ export class WebsiteMenuComponent implements OnInit {
 
     // Display profile image only if provided
     this.user$.subscribe((user) => {
-      if (user.profilePictureUri.length > 0)
-        this.image = user.profilePictureUri;
+      if (user.profilePicture !== null && user.profilePicture !== undefined)
+        this.image = user.profilePicture.sizes.filter((x) => x.width === 150)
+          .length
+          ? user.profilePicture.sizes.filter((x) => x.width === 150)[0].imageUrl
+          : user.profilePicture.imageUrl;
     });
 
     this.router.events.subscribe((nEvent) => {
